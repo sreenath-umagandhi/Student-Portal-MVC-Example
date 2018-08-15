d1ac587b91e5a29c2a922b12240f4e20193fd14f
sreenath-umagandhi <42354674+sreenath-umagandhi@users.noreply.github.com>
2018-08-15 11:08:45 -0500

    Student Portal MVC

---
+++ StudentRecords/StudentRecords/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace StudentRecords.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private SqlConnection conn;
+        private void connection()
+        {
+        //  string constr =  ConfigurationManager.ConnectionStrings["Data Source=.;Initial Catalog=StudentRecords;Integrated Security=True"].ToString();
+            conn = new SqlConnection("Data Source=IMMY-233\\SQLEXPRESS;Initial Catalog=StudentRecords;Integrated Security=True");
+        }
+
+
+        public ActionResult AddStudentInfo(Models.StudentRecord Stu)
+        {
+            if(ModelState.IsValid)
+            {
+
+                connection();
+                SqlCommand com = new SqlCommand("InsertData", conn);
+                com.CommandType = System.Data.CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("@studentID", Stu.studentID);
+                com.Parameters.AddWithValue("@studentName", Stu.studentName);
+                com.Parameters.AddWithValue("@studentAge", Stu.studentAge);
+                com.Parameters.AddWithValue("@studentMajor", Stu.studentMajor);
+
+                com.Parameters.AddWithValue("@studentGPA", Stu.studentGPA);
+                conn.Open();
+                
+                int i = com.ExecuteNonQuery();
+                conn.Close();
+                if (i>=1)
+                {
+                    ViewBag.Message = "New Student has been added";
+                    
+                }
+
+            }
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
---
+++ StudentRecords/StudentRecords/Models/StudentRecord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentRecords.Models
+{
+    public class StudentRecord
+    {
+        [Display(Name = "Student ID")]
+        [Required(ErrorMessage = "Enter Student ID")]
+        public string studentID { get; set; }
+
+        [Display(Name = "Student Name")]
+        public string studentName { get; set; }
+
+        [Display(Name = "Student Age")]
+        public string studentAge { get; set; }
+
+        [Display(Name = "Student Major")]
+        public string studentMajor { get; set; }
+
+        [Display(Name = "Student GPA")]
+        public string studentGPA { get; set; }
+
+
+    }
+
+}
